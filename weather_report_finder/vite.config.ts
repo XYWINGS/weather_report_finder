f2e9981d74c68e2d32c04b303218fa7ce944c3da
kanana <chalithbandara99@gmail.com>
2025-06-13 23:05:53 +0530

    add TypeScript node types and configure path aliases in Vite

--- weather_report_finder/vite.config.ts
+++ weather_report_finder/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@configs": path.resolve(__dirname, "./src/configs"),
+      "@slices": path.resolve(__dirname, "./src/slices"),
+      "@views": path.resolve(__dirname, "./src/views"),
+    },
+  },
+});
